88e406a9c50c0d063a1d3d1064b1b74faaec391d
新田光宏 <potimarimo@gmail.com>
2020-08-09 08:34:13 +0900

    データバインドの例を追加

---
+++ wpf-5.ps1
@@ -0,0 +1,11 @@
+﻿using namespace System.Xml
+
+Add-Type -AssemblyName PresentationFramework
+[xml]$xaml = Get-Content .\wpf-5.xaml
+$nodeReader = (New-Object XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlReader]::Load($nodeReader)
+$window.DataContext = [PSCustomObject]@{
+    FirstName = "太郎";
+    LastName = "佐藤"
+}
+$window.ShowDialog()
