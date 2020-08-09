617532badf1e558a57bf66722c7ae9df40d2df0f
新田光宏 <potimarimo@gmail.com>
2020-08-09 18:40:56 +0900

    PowerShellクラスでのデータバインド追加

---
+++ wpf-5-2.ps1
@@ -0,0 +1,19 @@
+﻿using namespace System.Xml
+
+Add-Type -AssemblyName PresentationFramework
+[xml]$xaml = Get-Content .\wpf-5.xaml
+$nodeReader = (New-Object XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlReader]::Load($nodeReader)
+
+class PSClass
+{
+    [string]$FirstName
+    [string]$LastName
+}
+
+$obj = New-Object PSClass
+$obj.FirstName = "太郎"
+$obj.LastName = "佐藤"
+
+$window.DataContext = $obj
+$window.ShowDialog()
