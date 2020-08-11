c6db6b091d5eb21977e02bb03c32a953e145d6b8
新田光宏 <potimarimo@gmail.com>
2020-08-12 03:14:21 +0900

    ObservableCollectionの例の追加

---
+++ wpf-7.ps1
@@ -0,0 +1,23 @@
+﻿using namespace System.Xml
+using namespace System.Dynamic
+using namespace System.Collections.ObjectModel
+
+function New-ExpandoObject([PSCustomObject]$source){
+    $source = [PSCustomObject]$source
+    $ret = New-Object ExpandoObject
+    $source | Get-Member | ?{$_.MemberType -eq "NoteProperty"} | %{$ret.($_.Name) = $source.($_.Name)}
+    return ,$ret
+}
+
+Add-Type -AssemblyName PresentationFramework
+[xml]$xaml = Get-Content .\wpf-7.xaml
+$nodeReader = (New-Object XmlNodeReader $xaml)
+$window = [Windows.Markup.XamlReader]::Load($nodeReader)
+$button = $window.FindName("button")
+$collection = New-Object ObservableCollection[ExpandoObject]
+$window.DataContext = $collection
+$button.add_Click({
+    $collection.Add((New-ExpandoObject @{FirstName="";LastName=""}))
+})
+$window.ShowDialog()
+
